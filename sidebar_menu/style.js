874f272602389a91c622144fc89647ad78049842
Seval Şentürk <sevalsenturk2017@gmail.com>
2023-02-20 11:48:10 +0300

    hamburger menu uygulama

---
+++ sidebar_menu/style.js
@@ -0,0 +1,9 @@
+/** for toggle start **/
+const $button  = document.querySelector('#sidebar-toggle');
+const $wrapper = document.querySelector('#wrapper');
+
+$button.addEventListener('click', (e) => {
+    e.preventDefault();
+    $wrapper.classList.toggle('toggled');
+});
+/** for toggle end **/
